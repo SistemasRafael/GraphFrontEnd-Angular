b55fd9873660ee0bcdd8281041b2f5d2f24f2b63
Rafael Flores Moreno <flores.freak@gmail.com>
2025-04-28 08:58:31 -0600

    instalationAngularMaterialSignIInForm

--- graph-ui/graph/src/app/app-routing.module.ts
+++ graph-ui/graph/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
-import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component : HomeComponent
-  },
-  {
-      path:'SignIn',
-      component : SignInComponent
+    path: 'auth',
+    loadChildren: () => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
   }
 ];
 
--- graph-ui/graph/src/app/app.component.ts
+++ graph-ui/graph/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'graph';
+  // public badgevisible = false;
+  badgevisibility() {
+    // this.badgevisible = true;
+  }
 }
