9b6cf16cb13a792c320dbde27f434dc905a921e6
Rafael Flores Moreno <flores.freak@gmail.com>
2025-04-30 11:59:42 -0600

    code changes

--- graph-ui/graph/src/app/layouts/admin/admin.component.spec.ts
+++ graph-ui/graph/src/app/layouts/admin/admin.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -8,7 +13,16 @@ describe('AdminComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AdminComponent]
+      declarations: [
+        AdminComponent
+      ],
+      imports: [
+        RouterModule.forRoot([]),
+        MatSidenavModule,
+        MatListModule,
+        MatToolbarModule,
+        MatIconModule
+      ],
     })
     .compileComponents();
 
--- graph-ui/graph/src/app/layouts/public/public.component.spec.ts
+++ graph-ui/graph/src/app/layouts/public/public.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PublicComponent } from './public.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 describe('PublicComponent', () => {
   let component: PublicComponent;
@@ -8,7 +13,16 @@ describe('PublicComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PublicComponent]
+      declarations: [
+        PublicComponent
+      ],
+      imports: [
+        RouterModule.forRoot([]),
+        MatSidenavModule,
+        MatListModule,
+        MatToolbarModule,
+        MatIconModule
+      ],
     })
     .compileComponents();
 
